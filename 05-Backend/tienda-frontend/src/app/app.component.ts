2e937240250d902e8bf19e754767cf5e5cf19e39
Gorky Muñoz <gorkymunoz@hotmail.com>
2019-06-17 17:48:27 -0600

    registrar usuario servicio

--- 05-Backend/tienda-frontend/src/app/app.component.ts
+++ 05-Backend/tienda-frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { UsuarioHttpService } from './servicios/http/usuario-http.service';
 
 @Component({
   selector: 'app-root',
@@ -10,23 +11,21 @@ import { environment } from 'src/environments/environment';
 export class AppComponent implements OnInit{
   title = 'tienda-frontend';
 
-  constructor(private readonly _HttpClient:HttpClient){
-
+  constructor(private readonly _HttpClient:HttpClient,
+              private readonly _usuarioHttpService:UsuarioHttpService){
   }
 
   ngOnInit(){
-    const url = environment.url + "/Usuario";
-    const listraUsuarios$ = this._HttpClient.get(url)
+    const usuarioCrear$ = this._usuarioHttpService
+    .crear({nombre:"Felipe",apellido:"Caicedo"});
 
-    listraUsuarios$.subscribe(
-      (datos)=>{
-        console.log(datos);
-    },
-    (error)=>{
-      console.log(error);
-    }
-    )
+    usuarioCrear$.subscribe(
+      (nuevoUsuario)=>{
+        console.log(nuevoUsuario);
+      },
+      (error)=>{
+        console.error(error);
+      }
+    );
   }
-
-
 }
--- 05-Backend/tienda-frontend/src/app/app.module.ts
+++ 05-Backend/tienda-frontend/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import {UsuarioHttpService} from './servicios/http/usuario-http.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import {HttpClientModule} from '@angular/common/http'
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [UsuarioHttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
