02dc552cb565664294aeb13c76e9f9fa2339601d
Gorky Muñoz <gorkymunoz@hotmail.com>
2019-06-24 16:31:24 -0600

    nuevos atributos a usuario

--- 05-Backend/tienda-backend/api/models/Usuario.ts
+++ 05-Backend/tienda-backend/api/models/Usuario.ts
@@ -9,11 +9,28 @@ module.exports = {
 
   attributes: {
     nombre:{
-      type:'string'
+      type:'string',
+      required: true,
+      minLength:3,
+      maxLength:60,
     },
     apellido:{
       type:'string',
       required:true
+    },
+    cedula:{
+      type:'string',
+      required:true,
+      unique:true,
+      minLength:10,
+      maxLength:25
+    },
+    username:{
+      type:'string',
+      required:true,
+      unique:true,
+      minLength:3,
+      maxLength:60,
     }
 
   },
