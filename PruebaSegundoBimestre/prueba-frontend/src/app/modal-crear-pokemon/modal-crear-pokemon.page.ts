0b196dd552c43f072a8d72ac88d97c0abe34a7a6
Gorky Muñoz <gorkymunoz@hotmail.com>
2019-07-28 16:00:46 -0600

    crear pokemon

--- PruebaSegundoBimestre/prueba-frontend/src/app/modal-crear-pokemon/modal-crear-pokemon.page.ts
+++ PruebaSegundoBimestre/prueba-frontend/src/app/modal-crear-pokemon/modal-crear-pokemon.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Pokemon} from "../dto/pokemon";
-import {ModalController, NavParams} from "@ionic/angular";
+import {ModalController, NavParams, ToastController} from "@ionic/angular";
+import {PokemonHttpService} from "../servicios/http/pokemon-http.service";
 
 @Component({
   selector: 'app-modal-crear-pokemon',
@@ -10,9 +11,28 @@ import {ModalController, NavParams} from "@ionic/angular";
 export class ModalCrearPokemonPage implements OnInit {
 
   nuevoPokemon = {} as Pokemon;
-  constructor(private readonly _modalController: ModalController, private readonly _navParams: NavParams,) { }
 
+  constructor(private readonly _modalController: ModalController,
+              private readonly _navParams: NavParams,
+              private readonly _toastController: ToastController,
+              private readonly _PokemonHttpService:PokemonHttpService) {
+  }
+
+  async presentToast(mensaje) {
+    const toast = await this._toastController.create({
+      message: mensaje,
+      duration: 1000
+    });
+    toast.present();
+  }
   guardarPokemon(){
+    const $nuevoPokemon = this._PokemonHttpService.crear(this.nuevoPokemon);
+    $nuevoPokemon.subscribe((value) => {
+      this.presentToast('Se creó el pokemon');
+      this.dismiss();
+    }, (error) => {
+      this.presentToast('Existió un problema al crear el pokemon');
+    });
   }
 
   dismiss(){
@@ -26,8 +46,6 @@ export class ModalCrearPokemonPage implements OnInit {
   ngOnInit() {
     this.nuevoPokemon.fkEntrenador = Number(this._navParams.get('idEntrenador'));
     console.log(this.nuevoPokemon.fkEntrenador);
-    //this.nuevoPokemon.fkEntrenador = 
-
   }
 
 }
