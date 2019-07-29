eed08b1f6f3b17ddae319904ebb18d7eddaba4c6
Gorky Muñoz <gorkymunoz@hotmail.com>
2019-07-28 20:09:42 -0600

    listar facturas

--- PruebaSegundoBimestre/prueba-frontend/src/app/tab3/tab3.page.ts
+++ PruebaSegundoBimestre/prueba-frontend/src/app/tab3/tab3.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CarritoCompras} from '../dto/carrito-compras';
+import {CarritoComprasHttpService} from '../servicios/http/carrito-compras-http.service';
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+    selector: 'app-tab3',
+    templateUrl: 'tab3.page.html',
+    styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+    listaFacturas: CarritoCompras[] = [];
+    parametroABuscar = '';
+
+    // tslint:disable-next-line:variable-name
+    constructor(private readonly _CarritoComprasHttpService: CarritoComprasHttpService) {
+    }
+
+    buscarFactura() {
+
+        
+
+    }
+
+    listarFacturas() {
+        const $facturas = this._CarritoComprasHttpService.buscarTodos();
+        $facturas.subscribe((value) => {
+            this.listaFacturas = value;
+        }, (error) => {
+            console.log('error ', error);
+        });
+    }
+
+    ngOnInit(): void {
+        this.listarFacturas();
+    }
 
 }
